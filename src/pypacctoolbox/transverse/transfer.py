98c3064a5433ee634a849ed16d43fae8ee9fb10c
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-10-08 19:52:43 +0100

    transverse: scoped math

--- src/pypacctoolbox/transverse/transfer.py
+++ src/pypacctoolbox/transverse/transfer.py
@@ -14,10 +14,12 @@ class cs_matrix(_elementBase):
 class twiss_matrix():
     def __init__(self, beta0, alpha0, gamma0, mu):
         self.m2x2 = _Matrix([[_cos(mu) + alpha0*_sin(mu), beta0*_sin(mu)],
-                             [-gamma0*_sin(mu), _cos(mu)-alpha0*sin(mu)]])
+                             [-gamma0*_sin(mu), _cos(mu)-alpha0*_sin(mu)]])
+
 class beta_matrix(_elementBase):
     def __init__(self, beta, alpha, gamma):
         self.m2x2 = _Matrix([[beta, -alpha],[-alpha, gamma]])
+
 class twiss_transform_matrix :
     def __init__(self, c,s, cp, sp):
         self.m2x2 = _Matrix([[c**2, -2*s*c, s**2],
