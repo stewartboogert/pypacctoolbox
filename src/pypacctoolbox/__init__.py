f22b0b1e5855818a6f58ea91baf6355f83f0d987
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-03-24 22:51:43 +0000

    multi-index

--- src/pypacctoolbox/__init__.py
+++ src/pypacctoolbox/__init__.py
@@ -1,3 +1,4 @@
+from . import math
 from . import base
 from . import element
 from . import relativity
---
+++ src/pypacctoolbox/math/multi_index.py
@@ -0,0 +1,20 @@
+#
+# list**mi
+# diff(expr, var, mi)
+#
+
+class MultiIndex(list) :
+
+    def __init__(self, index) :
+        list.__init__(self,index)
+
+
+    def diff(self, expr, vars):
+        pass
+
+    def factorial(self):
+        pass
+
+    def power(self, vars):
+        pass
+
