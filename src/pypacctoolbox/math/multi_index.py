2f9642c39a3cd2a9ebb81511080e43b49cbc6e63
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-03-26 22:37:57 +0000

    multi-index generator

--- src/pypacctoolbox/math/multi_index.py
+++ src/pypacctoolbox/math/multi_index.py
@@ -5,14 +5,15 @@
 
 import sympy as _sympy
 import math as _math
+import itertools as _itertools
 
-class MultiIndex(list) :
+class MultiIndex :
 
-    def __init__(self, index) :
-        list.__init__(self,index)
+    def __init__(self, indices) :
+        self.indices = indices
 
     def __add__(self, other) :
-        return MultiIndex([v1+v2 for v1, v2 in zip(self, other)])
+        return MultiIndex([v1+v2 for v1, v2 in zip(self.indices, other.indices)])
 
     def diff(self, exprs, vars):
         pass
@@ -20,15 +21,23 @@ class MultiIndex(list) :
     def norm(self):
         vsum = 0
 
-        for v in self:
+        for v in self.indices:
             vsum += v
 
         return vsum
 
+    def iterationCount(self):
+        vcount = 1
+
+        for v in self.indices:
+            vcount = vcount*v
+
+        return vcount
+
     def factorial(self):
         vprod = 1
 
-        for v in self:
+        for v in self.indices:
             vprod *=  _math.factorial(v)
 
         return vprod
@@ -40,11 +49,10 @@ class MultiIndex(list) :
         pass
 
     def power(self, vars):
-
         pass
 
     def __eq__(self, other) :
-        for v1,v2 in zip(self, other):
+        for v1,v2 in zip(self.indices, other.indices):
             if v1 != v2 :
                 return False
 
@@ -52,22 +60,54 @@ class MultiIndex(list) :
 
 
     def __lt__(self, other) :
-        for v1,v2 in zip(self, other):
+        for v1,v2 in zip(self.indices, other.indices) :
             if v1 >= v2 :
                 return False
 
         return True
 
     def __le__(self, other) :
-        for v1,v2 in zip(self, other):
+        for v1,v2 in zip(self.indices, other.indices) :
             if v1 > v2 :
                 return False
 
         return True
 
     def __gt__(self, other):
-        for v1,v2 in zip(self,other)
+        for v1,v2 in zip(self.indices,other.indices) :
             if v1 <= v2 :
                 return False
 
         return True
+
+    def __repr__(self):
+        return str(self.indices)
+
+    def __iter__(self):
+        # reset counter
+        self._cntr = 0
+
+        # make cartesian product
+        ranges = []
+        for i  in self.indices :
+            ranges.append(range(0,i))
+
+
+        self._cprod = [v for v in _itertools.product(*ranges)]
+
+        return self
+
+    def __next__(self) :
+
+        if self._cntr == self.iterationCount():
+            raise StopIteration
+
+        mi = MultiIndex(list(self._cprod[self._cntr]))
+
+        self._cntr += 1
+
+        return mi
+
+
+
+
