09a818b43d579a45bb38e33dd28fae3ef4e5661e
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-03-26 09:05:16 +0000

    multi index as general class

--- src/pypacctoolbox/math/multi_index.py
+++ src/pypacctoolbox/math/multi_index.py
@@ -3,18 +3,71 @@
 # diff(expr, var, mi)
 #
 
+import sympy as _sympy
+import math as _math
+
 class MultiIndex(list) :
 
     def __init__(self, index) :
         list.__init__(self,index)
 
+    def __add__(self, other) :
+        return MultiIndex([v1+v2 for v1, v2 in zip(self, other)])
 
-    def diff(self, expr, vars):
+    def diff(self, exprs, vars):
         pass
 
+    def norm(self):
+        vsum = 0
+
+        for v in self:
+            vsum += v
+
+        return vsum
+
     def factorial(self):
+        vprod = 1
+
+        for v in self:
+            vprod *=  _math.factorial(v)
+
+        return vprod
+
+    def binominal(self):
+        pass
+
+    def multinomial(self):
         pass
 
     def power(self, vars):
+
         pass
 
+    def __eq__(self, other) :
+        for v1,v2 in zip(self, other):
+            if v1 != v2 :
+                return False
+
+        return True
+
+
+    def __lt__(self, other) :
+        for v1,v2 in zip(self, other):
+            if v1 >= v2 :
+                return False
+
+        return True
+
+    def __le__(self, other) :
+        for v1,v2 in zip(self, other):
+            if v1 > v2 :
+                return False
+
+        return True
+
+    def __gt__(self, other):
+        for v1,v2 in zip(self,other)
+            if v1 <= v2 :
+                return False
+
+        return True
